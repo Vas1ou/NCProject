0439656e4d0a861befbd6e53e3dbebbb1636ddfd
Dmitry Vasilev <your_email@example.com>
2024-08-27 16:30:12 +0300

    added .env

--- app/applications/users/application_settings.py
+++ app/applications/users/application_settings.py
@@ -1,8 +1,13 @@
 from fastapi.security import OAuth2PasswordBearer
+from dotenv import load_dotenv
+import os
 
 # Конфигурация для JWT
-SECRET_KEY = "nord_clan_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 5
+# Загружаем переменные из .env файла
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token")
