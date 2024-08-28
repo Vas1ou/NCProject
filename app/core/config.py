361334de89e6754606612699725f85f699b9f5bd
Dmitry Vasilev <your_email@example.com>
2024-08-28 16:24:22 +0300

    added information about the database to .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,19 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные из .env файла
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Nord Clan Project"
-    DATABASE_URL: str = "postgresql+asyncpg://myuser:mypassword@localhost/mydatabase"
+    DATABASE_URL: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
 
 settings = Settings()
